99b00afa6faccb141842dbb7967eefecae4199aa
Sebastian Rendon <srendon521@gmail.com>
2025-02-17 12:57:59 -0500

    Creacion primera clase

--- app/notebook.py
+++ app/notebook.py
@@ -1,1 +1,29 @@
-# TODO: Agrega el código de las clases del modelo aquí. Borra este comentario al terminar.
+from datetime import datetime
+
+
+class Note:
+    HIGH: str = "HIGH"
+    MEDIUM: str = "MEDIUM"
+    LOW: str = "LOW"
+
+    def __init__(self, code: str, title:str, text: str, importance: str)
+        self.code : str = code
+        self.title : str = title
+        self.text: str = text
+        self.importance: str = importance
+        self.creation_date: datetime = datetime.now()
+        self.tags: list[str] = []
+
+    def add_tag(self, tag: str):
+        if tag not in self.tags:
+            self.tags.append(tag)
+
+    def __str__(self) -> str:
+        return f"Date: {self.creation_date} {self.title}: {self.text}"
+
+
+
+
+
+
+
