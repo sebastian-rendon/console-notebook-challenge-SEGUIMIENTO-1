0bab350c74d01d3a5546bebc294597dd67407bbc
Sebastian Rendon <srendon521@gmail.com>
2025-02-17 13:35:25 -0500

    Avance la clase Notebook

--- app/notebook.py
+++ app/notebook.py
@@ -22,6 +22,35 @@ class Note:
         return f"Date: {self.creation_date} {self.title}: {self.text}"
 
 
+class Notebook:
+    def __init__(self):
+        self.notes: list[Note] = []
+
+    def add_note(self, title: str, text: str, importance: str) -> int:
+        code = len(self.notes) + 1
+        new_note = Note(code, title, text, importance)
+        self.notes.append(new_note)
+        return code
+
+    def delete_note(self, code: int):
+        self.notes = [note for note in self.notes if note.code != code]
+
+    def important_notes(self):
+        return [note for note in self.notes if note.importance in {Note.HIGH, Note.MEDIUM}]
+
+    def notes_by_tag(self, tag: str):
+        return [note for note in self.notes if tag in note.tags]
+
+
+
+
+
+
+
+
+
+
+
 
 
 
